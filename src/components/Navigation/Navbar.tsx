d830eb567990a43b659682f327720807124d7fcb
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-03-14 22:32:45 +0530

    NavBar and Hero Section responsiveness

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../assets/aktechtbg.webp';
 import { NavbarItems } from '../../types';
 
@@ -48,22 +48,39 @@ const Navbar: React.FC = () => {
       item: 'About',
     },
   ];
+
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
+  const toggleSelect = () => {
+    setIsSelected((prev) => !prev);
+  };
+
+  // useEffect(() => {
+  //   const closeIfDropDownOpen = () => {
+  //     setIsSelected(false);
+  //   };
+  //   document.addEventListener('click', closeIfDropDownOpen);
+  //   return () => {
+  //     document.removeEventListener('click', closeIfDropDownOpen);
+  //   };
+  // }, []);
+
   return (
     <nav className="border-gray-200 bg-black/40 shadow-lg shadow-white/50 text-white">
-      <div className="flex flex-wrap items-center gap-2 mx-auto px-2">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <div className="flex flex-wrap lg:flex-nowrap items-center gap-2 mx-auto px-2 min-h-[60px]">
+        <a href="#" className="flex items-center space-x-2 rtl:space-x-reverse">
           <img src={Logo} className="h-8" alt="AK Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap">AK Travels</span>
+          <span className="self-center text-xl md:text-lg lg:text-sm xl:text-xl font-semibold whitespace-nowrap">AK Travels</span>
         </a>
-        <div className="hidden lg:block w-auto pl-8" id="navbar-dropdown">
+        <div className="hidden text-md md:text-sm lg:text-2md lg:block w-auto xl:pl-8" id="navbar-dropdown">
           <ul className="flex items-center font-medium p-0 border-gray-100 rounded-lg space-x-6 rtl:space-x-reverse flex-row mt-0 border-0">
             {navItems.map((navItem) => (
-              <li key={navItem.id} className={`relative flex items-center h-[60px] m-0 ${navItem.hasDropdown ? 'group' : ''}`}>
+              <li key={navItem.id} onClick={navItem.hasDropdown ? toggleSelect : undefined} className={`relative flex items-center h-[60px] m-0 ${navItem.hasDropdown ? 'group' : ''}`}>
                 <a href="#" className="flex items-center transition-all duration-300 rounded-sm border-0 hover:text-blue-500 p-6 h-[50%] hover:bg-black/40 hover:scale-115">
                   {navItem.item}
                 </a>
                 {navItem.hasDropdown && (
-                  <div className="hidden group-hover:block hover:block absolute top-15 rounded w-50 bg-black/40 overflow-hidden">
+                  <div className={`${isSelected ? 'block' : 'hidden'} group-hover:block hover:block absolute top-15 rounded w-50 bg-black/40 overflow-hidden`}>
                     <ul className="cursor-pointer">
                       {navItem.subNavItems!.map((subNavItem) => (
                         <li key={subNavItem.id} className="p-4 rounded transition-all duration-300 hover:text-blue-500 hover:bg-black/40 hover:scale-110">
@@ -77,7 +94,7 @@ const Navbar: React.FC = () => {
             ))}
           </ul>
         </div>
-        <div className="flex flex-row gap-6 ml-auto mr-6">
+        <div className="hidden lg:flex flex-row gap-6 ml-auto mr-6">
           <button
             type="button"
             className="w-30 transition-all duration-300 text-blue-700 bg-gradient-to-r from-slate-50 to-blue-200 hover:bg-gradient-to-bl hover:scale-105 focus:ring-3 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer"
