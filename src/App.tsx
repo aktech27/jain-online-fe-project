ca840932992af31876af4cb67eda2fa737176f56
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-03-19 22:00:53 +0530

    Modal changes -remove from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Navbar, HeroSection, BookNow, Packages, Services, Gallery, About, Footer } from './components';
-import Modal from './components/General/Modal';
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
       <Gallery />
       <About />
       <Footer />
-      <Modal />
     </>
   );
 };
