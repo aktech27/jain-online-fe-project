998f871a08c61d2e608a6133b1c67b584c03f712
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-03-26 22:26:36 +0530

    [WIP] Register form: form submit

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -1,9 +1,46 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Logo from '../../assets/aktechtbg.webp';
 import { ModalContext } from '../../context';
 
 const RegisterForm = () => {
   const { handleOpen } = useContext(ModalContext);
+  const defaultRegisterFormState = {
+    name: '',
+    dob: '',
+    email: '',
+    contact: '',
+    password: '',
+  };
+  const defaultRegisterFormErrors = {
+    name: '',
+    dob: '',
+    email: '',
+    contact: '',
+    password: '',
+  };
+
+  const [formState, setFormState] = useState(defaultRegisterFormState);
+  const [formErrors, setFormErrors] = useState(defaultRegisterFormErrors);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
+  const handleFullFormValidation = () => {
+    for (const [field, value] of Object.entries(formState)) {
+      console.log(field, value);
+      setFormErrors((prev) => ({ ...prev }));
+    }
+  };
+
+  const handleChange = (field: string, value: string) => {
+    setFormState((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
   return (
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
@@ -13,8 +50,8 @@ const RegisterForm = () => {
       <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Setup your new account</h1>
-          <form className="space-y-4 md:space-y-6" action="#">
-            <div>
+          <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+            <div className="relative">
               <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900">
                 Full Name
               </label>
@@ -25,9 +62,12 @@ const RegisterForm = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                 placeholder="Your Full Name"
                 required={true}
+                value={formState.name}
+                onChange={(e) => handleChange('name', e.target.value)}
               />
+              <div className="absolute text-xs ml-2 text-red-600 h-[15px]">{formErrors.name}</div>
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="dob" className="block mb-2 text-sm font-medium text-gray-900">
                 D.O.B
               </label>
@@ -38,9 +78,12 @@ const RegisterForm = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                 placeholder="Your Full Name"
                 required={true}
+                value={formState.dob}
+                onChange={(e) => handleChange('dob', e.target.value)}
               />
+              <div className="absolute text-xs ml-2 text-red-600 h-[15px]">{formErrors.dob}</div>
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
                 Email
               </label>
@@ -51,9 +94,12 @@ const RegisterForm = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                 placeholder="Your Email"
                 required={true}
+                value={formState.email}
+                onChange={(e) => handleChange('email', e.target.value)}
               />
+              <div className="absolute text-xs ml-2 text-red-600 h-[15px]">{formErrors.email}</div>
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="contact" className="block mb-2 text-sm font-medium text-gray-900">
                 Contact number
               </label>
@@ -64,9 +110,12 @@ const RegisterForm = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                 placeholder="Your Contact Number"
                 required={true}
+                value={formState.contact}
+                onChange={(e) => handleChange('contact', e.target.value)}
               />
+              <div className="absolute text-xs ml-2 text-red-600 h-[15px]">{formErrors.contact}</div>
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
                 Password
               </label>
@@ -77,10 +126,14 @@ const RegisterForm = () => {
                 placeholder="Your Password"
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                 required={true}
+                value={formState.password}
+                onChange={(e) => handleChange('password', e.target.value)}
               />
+              <div className="absolute text-xs ml-2 text-red-600 h-[15px]">{formErrors.password}</div>
             </div>
             <button
               type="submit"
+              onClick={() => handleFullFormValidation()}
               className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               Sign up
